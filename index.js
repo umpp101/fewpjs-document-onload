d126bb92a3c4183441ed4f6ba973b19206f2f327
Mustafa Muse <umpp101@icloud.com>
2019-12-09 17:01:28 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    update();
+  });
+   
+
+function update(){
+     document.querySelector("p").innerHTML = "This is really cool!"
+}
